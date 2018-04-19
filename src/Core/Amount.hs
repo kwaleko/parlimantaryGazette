8089feeeef103c68fcebee52303dbceb677301b2
kwaleko <khaledalomar@tutanota.com>
2018-04-19 22:24:29 +0300

    update names

--- src/Core/Amount.hs
+++ src/Core/Amount.hs
@@ -1,38 +1,44 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module Value
-  (Value(..)
+module Core.Amount
+  (Amount
   ,Error(..)
+  ,mkAmount
   )where
 
 
-newtype Value = Value (Either Error Int) deriving Eq
+newtype Amount = A (Either Error Int) deriving (Eq,Ord)
 
 data Error = NegativeNumber
            | CouldNotBeFractional
-           deriving Eq
+           deriving (Eq,Ord)
 
-instance Num Value where
-  Value a + Value b     = Value $ (+) <$> a <*> b
-  Value a - Value b     = Value $ (-) <$> a <*> b
-  Value a * Value b     = Value $ (*) <$> a <*> b
-  abs (Value a)         = Value $ abs <$> a
-  signum (Value a)       = Value $ signum <$> a
-  fromInteger a = Value $ pure  $ fromInteger a
+mkAmount ::  Int -> Amount
+mkAmount a = if a < 0
+                then A $ Left NegativeNumber
+                else A $ Right a
 
-instance Fractional Value where
-  Value (Left err) / _                = Value (Left err)
-  _                / Value (Left err) = Value (Left err)
-  Value (Right a)  / Value (Right b)  = Value $
+instance Num Amount where
+  A a + A b     = A $ (+) <$> a <*> b
+  A a - A b     = A $ (-) <$> a <*> b
+  A a * A b     = A $ (*) <$> a <*> b
+  abs (A a)     = A $ abs <$> a
+  signum (A a)  = A $ signum <$> a
+  fromInteger a = A $ pure  $ fromInteger a
+
+instance Fractional Amount where
+  A (Left err) / _                = A (Left err)
+  _                / A (Left err) = A (Left err)
+  A (Right a)  / A (Right b)  = A $
     if a `rem` b == 0 || 2 * (a `rem` b ) < b
                 then Right $  a `div` b
                 else Right $ (+1) $ a `div` b
-  fromRational a = Value $ Left CouldNotBeFractional
+  fromRational a = A $ Left CouldNotBeFractional
 
 
-instance Show Value where
+instance Show Amount where
   show value = case value of
-    Value (Left NegativeNumber) -> "Number is negative"
-    Value (Left CouldNotBeFractional) -> "could not be fractional"
-    Value (Right a)             -> show a
+    A (Left NegativeNumber)       -> "Number is negative"
+    A (Left CouldNotBeFractional) -> "could not be fractional"
+    A (Right a)                   -> show a
 
 
